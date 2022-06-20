f24857d7e96c8ba39c17c01fb1b33f2519251dfc
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-06-20 23:27:37 +0100

    Stopped using public vars for packages subsets

--- app/Traits/Packages.php
+++ app/Traits/Packages.php
@@ -16,26 +16,26 @@ trait Packages
         $this->info('Collecting packages...');
 
         // Get composer packages.
-        $this->packagesComposer = Collection::wrap($this->config['packages']['composer'])
+        $packagesComposer = Collection::wrap($this->config['packages']['composer'])
                                         ->whereInStrict('key', $this->config['packages-to-install']['composer']);
 
-        if ($this->packagesComposer->count() != count($this->config['packages-to-install']['composer'])) {
+        if ($packagesComposer->count() != count($this->config['packages-to-install']['composer'])) {
             $this->error('You are trying to install composer packages but you have not set some composer packages you require in the config.');
             exit;
         }
 
         // Get npm packages.
-        $this->packagesNpm = Collection::wrap($this->config['packages']['npm'])
+        $packagesNpm = Collection::wrap($this->config['packages']['npm'])
                                         ->whereInStrict('key', $this->config['packages-to-install']['npm']);
 
-        if ($this->packagesNpm->count() != count($this->config['packages-to-install']['npm'])) {
+        if ($packagesNpm->count() != count($this->config['packages-to-install']['npm'])) {
             $this->error('You are trying to install npm packages but you have not set some npm packages you require in the config.');
             exit;
         }
 
         $this->packages = Collection::wrap([
-            'composer' => $this->packagesComposer,
-            'npm' => $this->packagesNpm,
+            'composer' => $packagesComposer,
+            'npm' => $packagesNpm,
         ]);
 
         $this->info('Packages collected.');
