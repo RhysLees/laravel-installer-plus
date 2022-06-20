5b4d42c83a9b7e0e7b0220adf62acaac6a8cb118
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-06-20 23:26:56 +0100

    Changed Execute to use Symphony Process

--- app/Traits/Execute.php
+++ app/Traits/Execute.php
@@ -2,6 +2,9 @@
 
 namespace App\Traits;
 
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
 trait Execute
 {
     /**
@@ -10,19 +13,35 @@ trait Execute
      * @param string $command
      * @param bool $location
      *
-     * @return mixed $output
+     * @return Process $process
      */
     public function executeCommand($command, $location = false)
     {
         $path = $this->applicationLocation;
 
         if ($location) {
-            $path = $this->config['install-location'];
+            $path = $this->installLocation;
         }
 
-        exec('cd ' . $path .  ' && ' . $command, $output, $result);
-        
-        return $output;
+        $command = 'cd ' . $path .  ' && ' . $command;
+
+        $this->info('Executing: ' . $command);
+
+        $process = Process::fromShellCommandline($command, null, $env = [], null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->error('Warning: ' . $e->getMessage());
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->info('    ' . $line);
+        });
+
+        return $process;
     }
 
     /**
@@ -31,12 +50,12 @@ trait Execute
      * @param array $commands
      * @param bool $location true=install-location | false=application-location
      *
-     * @return mixed $output
+     * @return Process $process
      */
     public function executeCommands($commands, $location = false)
     {
-        $output = $this->executeCommand(implode(' && ', $commands), $location);
+        $process = $this->executeCommand(implode(' && ', $commands), $location);
 
-        return $output;
+        return $process;
     }
 }
